c40691133011e20a7544ebc70efee7d56879bb16
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-22 22:01:43 +0800

    [Story6]feat:case1,should return ticket and lotNumber1 when two parkingLots availablePositionRate same given a car

---
+++ src/main/java/com/parkinglot/SuperSmartParkingBoy.java
@@ -0,0 +1,22 @@
+package com.parkinglot;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SuperSmartParkingBoy extends ParkingBoy{
+    private float availablePositionRate;
+
+    @Override
+    public Ticket park(Car car) {
+        List<ParkingLot> isAvailableParkingLots = getParkingLots().stream()
+                .filter(parkingLot -> parkingLot.getEmptyPosition() >0)
+                .sorted(Comparator.comparing(ParkingLot::getAvailablePositionRate).reversed())
+                .collect(Collectors.toList());
+        if (isAvailableParkingLots.size() != 0) {
+            Ticket park = isAvailableParkingLots.get(0).park(car);
+            return park;
+        }
+        return getParkingLots().get(0).park(car);
+    }
+}
---
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -0,0 +1,24 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SuperSmartParkingBoyTest {
+    @Test
+    void should_return_ticket_and_lotNumber1_when_two_parkingLots_availablePositionRate_same_given_a_car() {
+        //Given
+        SuperSmartParkingBoy  superSmartParkingBoy = new SuperSmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot(10, 1);
+        ParkingLot secondParkingLot = new ParkingLot(10, 2);
+         superSmartParkingBoy.addParkingLots(firstParkingLot);
+         superSmartParkingBoy.addParkingLots(secondParkingLot);
+        Car car = new Car();
+        //When
+        Ticket ticket =  superSmartParkingBoy.park(car);
+        //Then
+        assertNotNull(ticket);
+        assertEquals(ticket.getParkingLotNumber(), 1);
+}
+}
